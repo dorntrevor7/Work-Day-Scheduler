44974f6b2caaf147ab9396a2969acbe8dfd5025d
Trevor Dorn <dorntrevor7@gmail.com>
2020-02-13 17:33:49 -0700

    Readme all ids with local stor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+
+In this project I created a work day scheduler with [Bootstrap](https://getbootstrap.com/) and [moment.js](https://momentjs.com/) that allows you to:
+
+* Save important things/ tasks you need to get done within the time frame
+* Display the saved text no matter if you close out the tab
+* Knows the time and day:
+
+    * Grey = Past
+    * Red = Present
+    * Green = future
+
+Here's the link: [Work Day Scheduler](https://dorntrevor7.github.io/Work-Day-Scheduler/)
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var otherHour = moment().hours();
-console.log(otherHour);
+
+// Displays the current day in the class jumbrotron
+var currentDay = $("<p>").text(moment().format("MMMM Do YYYY"));
+currentDay.addClass("lead");
+$(".jumbotron").append(currentDay);
+
 // creating getting the attribute of the data number and 
 // displaying the color in the right time slot/box
 $(".hour").each(function () {
@@ -8,10 +12,11 @@ $(".hour").each(function () {
         .removeClass("future")
         .removeClass("past");
 
+    // creating variables for the moment methods 
+    // of getting the hour that your computer is currently in and 
+    // checking to see if the hour is past, present, or future
     var currentHour = moment().format("h");
     var dataNum = $(this).attr("data-num");
-    // console.log(dataNum);
-    // console.log(currentHour);
 
     if (dataNum < currentHour) {
         $(this).addClass("past");
@@ -24,17 +29,29 @@ $(".hour").each(function () {
     };
 });
 
+// When the save button gets clicked
 $(".saveBtn").on("click", function () {
-    //console.log($(this));
+
+    // The saveBtn gets the value of text from the from its sibling 
+    // in the textarea and pairs it with the hour that is displayed
     var text = $(this).siblings("textarea").val().trim();
     var hour = $(this).siblings("textarea").attr("data-num");
     console.log("text: ", text);
     console.log("hour: ", hour);
 
-    //store in local storage
+    // Store in local storage
     localStorage.setItem(hour, text);
-
 })
 
-//getting texts from local stroage
+//getting the texts that are stored in the keys
+// from local stroage and 
+// tracing it back to the certain Id
 $("#9").text(localStorage.getItem("09"));
+$("#10").text(localStorage.getItem("10"));
+$("#11").text(localStorage.getItem("11"));
+$("#12").text(localStorage.getItem("12"));
+$("#1").text(localStorage.getItem("1"));
+$("#2").text(localStorage.getItem("2"));
+$("#3").text(localStorage.getItem("3"));
+$("#4").text(localStorage.getItem("4"));
+$("#5").text(localStorage.getItem("5"));
